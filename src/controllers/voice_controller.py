024a59d7e0b41e2decf9087c5c4fc92c2b2985f8
GO <boyd083@csusm.edu>
2025-04-20 20:02:30 -0700

    fix

--- src/controllers/voice_controller.py
+++ src/controllers/voice_controller.py
@@ -29,7 +29,7 @@ from collections.abc import Callable    # Function type hinting
 import json                             # Loading in custom commands
 from jsonschema import validate, ValidationError
 
-import src.command.command_maker as cm
+import command_controller as cm
 
 '''
 Make a button that opens an overlay
